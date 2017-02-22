87ce14a5194047b79658bdfc805f9aae11fcd57f
naesheim <hg.nesheim@gmail.com>
2017-02-22 14:19:23 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+echo "\n new build : $(date)" >> output/file.txt
---
+++ init.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+echo "artifactory_user=artifactdeployer" >> ~/.gradle/gradle.properties
+echo "artifactory_password=praqma" >> ~/.gradle/gradle.properties
+echo "artifactory_contextUrl=http\://127.0.0.1\:8081/artifactory" >> ~/.gradle/gradle.properties
